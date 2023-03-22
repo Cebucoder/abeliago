ded28c843a8cca7c65f555d9099a6c5fa8c3da32
Cebucoder <danielabellana187@gmail.com>
2023-03-22 19:12:58 +0800

    fixing cart

--- js/cart-script.js
+++ js/cart-script.js
@@ -19,36 +19,54 @@ function addToCart(img,name,price){
 
 let cartContainer = document.getElementById("cartItems");
 
-function showCarts(){
-	// Clear the cart container
-	cartContainer.innerHTML = "";
+function showCarts() {
+  // Clear the cart container
+  cartContainer.innerHTML = "";
 
-	// Iterate over the items in the cart and add them to the display
-	carts.forEach(function(cartItems){
-		cartContainer.innerHTML +=
-			`
-			<li>
-				<div class="cart-img">
-					<img src="${cartItems.productImg}">
-				</div>
-				<div class="item-name">
-					<label>${cartItems.productName}</label>
-				</div>
-				<span class="item-price">${cartItems.productPrice}</span>
-			</li>
-			`;
-	});
+  // Initialize the itemNumber variable to 0
+  let itemNumber = 0;
 
-	// Calculate the total and display it
-	let totalAmount = 0;
-	carts.forEach(function(cartItems){
-		totalAmount += Number(cartItems.productPrice);
-	});
-	document.getElementById("total").innerHTML = "₱"+totalAmount.toLocaleString();
+  // Iterate over the items in the cart and add them to the display
+  carts.forEach(function(cartItems) {
+    // Increment the itemNumber for each item in the cart
+    itemNumber++;
+
+    cartContainer.innerHTML +=
+      `
+      <li>
+        <div class="cart-img">
+          <a href= "cart.html"><img src="${cartItems.productImg}"></a>
+        </div>
+        <div class="item-name">
+          <a href="cart.html"><label>${cartItems.productName}</label></a>
+        </div>
+        <span class="item-price">₱ ${cartItems.productPrice}</span>
+        <span class="trash" onclick="removeItem(${itemNumber - 1})"><ion-icon name="trash-outline"></ion-icon></span>
+      </li>
+      `;
+  });
+
+  // Calculate the total and display it
+  let totalAmount = 0;
+  carts.forEach(function(cartItems) {
+    totalAmount += Number(cartItems.productPrice);
+  });
+  document.getElementById("total").innerHTML = "₱" + totalAmount.toLocaleString();
 }
+
 
 // Load the cart from localStorage when the page is loaded
 carts = JSON.parse(localStorage.getItem("cartStorage")) || [];
 
 // Display the cart
 showCarts();
+
+
+function removeItem(itemNumber) {
+  let cartStorages = JSON.parse(localStorage.getItem("cartStorage")) || [];
+  cartStorages.splice(itemNumber, 1);
+  localStorage.setItem("cartStorage", JSON.stringify(cartStorages));
+  carts = cartStorages;
+  showCarts();
+}
+
